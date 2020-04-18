f89b4d4bf7440a4f83d741a1ed73a216339bf84f
elthas <elthas@outlook.be>
2020-04-18 21:25:52 +0200

    removed hostame stuff, doesnt work in service mode?

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 import socket
 import os
 from flask import Flask
+from datetime import datetime
+
+
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    host_name = socket.gethostname()
-    host_ip = socket.gethostbyname(host_name)
+    # host_name = socket.gethostname()
+    # host_ip = socket.gethostbyname(host_name)
     message = 'my first message'
-    return "IP: {ip}, hostname {h}, message: {m}".format(ip=host_ip, h=host_name, m=message)
+    now = datetime.now()
+
+    current_time = now.strftime("%H:%M:%S")
+    return "Message: {m} on {t}".format( m=message, t=now)
 
 if __name__ == "__main__":
     app.run('0.0.0.0')
