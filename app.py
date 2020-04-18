b80446c0c42bb0bcab89f4a705d7fb078d70a460
elthas <elthas@outlook.be>
2020-04-18 21:12:45 +0200

    added a message

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ app = Flask(__name__)
 def hello():
     host_name = socket.gethostname()
     host_ip = socket.gethostbyname(host_name)
-    return "IP: {ip}, hostname {h}".format(ip=host_ip, h=host_name)
+    message = 'my first message'
+    return "IP: {ip}, hostname {h}, message: {m}".format(ip=host_ip, h=host_name, m=message)
 
 if __name__ == "__main__":
     app.run('0.0.0.0')
